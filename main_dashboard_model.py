f6f5d4b588fcd83f27dff608a25b8b0c5752052c
Matthew Laster <matt.laster@modelop.com>
2022-06-01 00:25:23 -0400

    lab two build

--- main_dashboard_model.py
+++ main_dashboard_model.py
@@ -235,6 +235,7 @@ def metrics(baseline, comparator) -> dict:
                 for i, item in enumerate(evaluated_results):
                     if item.get("monitor name") == "Service Response Time":
                         break
+                    LOG.info("Service Response Time object found at index: " + i)
                 obj = {'color':"Green",'monitor name':"Service Response Time"}    
                 evaluated_results[i] = obj
             else:
@@ -243,6 +244,7 @@ def metrics(baseline, comparator) -> dict:
                 for i, item in enumerate(evaluated_results):
                     if item.get("monitor name") == "Service Response Time":
                         break
+                    LOG.info("Service Response Time object found at index: " + i)
                 obj = {'color':"Red",'monitor name':"Service Response Time"}    
                 evaluated_results[i] = obj
             LOG.info(evaluated_results)
