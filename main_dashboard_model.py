0d253641fd2525d5b69c2b19b8c9d931fde25403
Matthew Laster <matt.laster@modelop.com>
2023-12-19 17:34:36 -0500

    updating code

--- main_dashboard_model.py
+++ main_dashboard_model.py
@@ -1,5 +1,7 @@
 import json
+import re
 from datetime import datetime
+from typing import List, Optional, Dict
 
 import modelop.schema.infer as infer
 import modelop.utils as utils
@@ -10,7 +12,6 @@ from modelop_sdk.utils import dashboard_utils, dict_utils
 import modelop_sdk.apis.model_manage_api as model_manage
 import uuid
 from dateutil.parser import parse
-
 
 LOG = logger.configure_logger()
 
@@ -38,77 +39,160 @@ def init(job_json):
 
 # modelop.metrics
 def metrics(baseline, comparator) -> dict:
-    result = {}
-    heat_map = {}
-    flat_heatmap = {}
-    execution_errors_array = []
-    results_with_defaults = {
-        "CSI_maxCSIValue": -99,
-        "auc": -99,
-        "r2_score": 99,
-        "Bias_maxPPRDisparityValue": -99,
-        "Bias_minPPRDisparityValue": -99,
-        "DataDrift_maxKolmogorov-SmirnovPValue": -99,
-        "ConceptDrift_maxKolmogorov-SmirnovPValueValue": -99
+    # result = {}
+    # heat_map = {}
+    # flat_heatmap = {}
+    # execution_errors_array = []
+    # results_with_defaults = {
+    #     "CSI_maxCSIValue": -99,
+    #     "auc": -99,
+    #     "r2_score": 99,
+    #     "Bias_maxPPRDisparityValue": -99,
+    #     "Bias_minPPRDisparityValue": -99,
+    #     "DataDrift_maxKolmogorov-SmirnovPValue": -99,
+    #     "ConceptDrift_maxKolmogorov-SmirnovPValueValue": -99
+    # }
+
+    # client = moc_client.MOCClient()
+    # mtr_summary_api = model_manage.ModelTestResultSummariesApi(client)
+    # mtr_api = model_manage.ModelTestResultsApi(client)
+    # mlc_api = mlc.MLCApi(client)
+
+    # def get_associated_model_latest_mtrs_mapper(model_association):
+    #     associated_model_id = uuid.UUID(model_association.get("associatedModel", {}).get("id"))
+    #     associated_model_name = model_association.get("associatedModel", {}).get("storedModel", {}).get("modelMetaData", {}).get("name", "No name found")
+    #     response_of_model_test_result_summaries = mtr_summary_api.find_all_by_optional(
+    #         deployable_model_id=uuid.UUID(DEPLOYABLE_MODEL.get("id")),
+    #         associated_model_snapshot_id=associated_model_id,
+    #         sort="createdDate,desc", page=0, size=1)
+    #     if len(response_of_model_test_result_summaries.get("_embedded", {}).get("modelTestResultSummaries", [])) > 0:
+    #         mtr_summary = response_of_model_test_result_summaries.get("_embedded", {}).get("modelTestResultSummaries", [])[0]
+    #         mtr = mtr_api.get(mtr_summary.get("id"))
+    #         return mtr
+    #     else:
+    #         LOG.error(f"Couldn't find Model Test Result for deployableModel " + str(DEPLOYABLE_MODEL.get("id")) +
+    #                   " and associated model " + str(associated_model_name) + " (" + str(associated_model_id) + ")")
+    #         return None
+
+    # LOG.info("Number of associated models in the business model: " + str(len(DEPLOYABLE_MODEL.get("associatedModels", []))))
+    # if len(DEPLOYABLE_MODEL.get("associatedModels", [])) == 0:
+    #     raise Exception("No associated models were found in the current business model ")
+
+    # mtrs = list(map(get_associated_model_latest_mtrs_mapper, DEPLOYABLE_MODEL.get("associatedModels", [])))
+    # mtrs = list(filter(None, mtrs))
+    # result = {}
+    # result.update(combine_model_test_results(mtrs))
+    # result.update(extract_model_fields(execution_errors_array))
+
+    # try:
+    #     results_with_defaults.update(result)
+    #     LOG.info("Performing DMN evaluation")
+    #     job = json.loads(JOB.get("rawJson", {}))
+    #     evaluated_results = mlc_api.evaluate_results(results_with_defaults, "dashboard_model.dmn", additional_assets=job.get("additionalAssets", []))
+    #     LOG.info("Generating heatMap")
+    #     heat_map["heatMap"] = dashboard_utils.generate_heatmap(evaluated_results)
+
+    #     flat_heatmap = dict_utils.flatten_data(heat_map)
+    # except Exception as eval_ex:
+    #     heat_map = {"heatMap": {}}
+    #     LOG.error(str(eval_ex))
+    #     execution_errors_array.append(
+    #         "Something went wrong during DMN evaluation or heatmap generation, please check logs"
+    #     )
+
+    # value_risk = calculate_risk_count()
+
+    # dashboard_result = {"createdDate": datetime.now().strftime("%m/%d/%Y %H:%M:%S")}
+    # dashboard_result.update(results_with_defaults)
+    # dashboard_result.update(dashboard_result)
+    # dashboard_result.update(heat_map)
+    # dashboard_result.update(flat_heatmap)
+    # dashboard_result.update({"executionErrors": execution_errors_array})
+    # dashboard_result.update({"executionErrorsCount": len(execution_errors_array)})
+
+    # dashboard_result.update(calculate_custom_columns([
+    #     generate_custom_entry([preformatted(DEPLOYABLE_MODEL.get("metaData", {}).get("modelStage", "None"))],
+    #                           ["Model Stage"],
+    #                           None,
+    #                           name="Approved Use")
+    # ]))
+
+    #yield dashboard_result
+    dashboard_result = {
+        "createdDate": datetime.now().strftime('%m/%d/%Y %H:%M:%S'),
+        "modelUseCategory": "INSURANCE",
+        "modelOrganization": "Claims Processing",
     }
-
-    client = moc_client.MOCClient()
-    mtr_summary_api = model_manage.ModelTestResultSummariesApi(client)
-    mtr_api = model_manage.ModelTestResultsApi(client)
-    mlc_api = mlc.MLCApi(client)
-
-    def get_associated_model_latest_mtrs_mapper(model_association):
-        associated_model_id = uuid.UUID(model_association.get("associatedModel", {}).get("id"))
-        associated_model_name = model_association.get("associatedModel", {}).get("storedModel", {}).get("modelMetaData", {}).get("name", "No name found")
-        response_of_model_test_result_summaries = mtr_summary_api.find_all_by_optional(
-            deployable_model_id=uuid.UUID(DEPLOYABLE_MODEL.get("id")),
-            associated_model_snapshot_id=associated_model_id,
-            sort="createdDate,desc", page=0, size=1)
-        if len(response_of_model_test_result_summaries.get("_embedded", {}).get("modelTestResultSummaries", [])) > 0:
-            mtr_summary = response_of_model_test_result_summaries.get("_embedded", {}).get("modelTestResultSummaries", [])[0]
-            mtr = mtr_api.get(mtr_summary.get("id"))
-            return mtr
-        else:
-            LOG.error(f"Couldn't find Model Test Result for deployableModel " + str(DEPLOYABLE_MODEL.get("id")) +
-                      " and associated model " + str(associated_model_name) + " (" + str(associated_model_id) + ")")
-            return None
-
-    LOG.info("Number of associated models in the business model: " + str(len(DEPLOYABLE_MODEL.get("associatedModels", []))))
-    if len(DEPLOYABLE_MODEL.get("associatedModels", [])) == 0:
-        raise Exception("No associated models were found in the current business model ")
-
-    mtrs = list(map(get_associated_model_latest_mtrs_mapper, DEPLOYABLE_MODEL.get("associatedModels", [])))
-    mtrs = list(filter(None, mtrs))
-    result = {}
-    result.update(combine_model_test_results(mtrs))
-    result.update(extract_model_fields(execution_errors_array))
-
-    try:
-        results_with_defaults.update(result)
-        LOG.info("Performing DMN evaluation")
-        job = json.loads(JOB.get("rawJson", {}))
-        evaluated_results = mlc_api.evaluate_results(results_with_defaults, "dashboard_model.dmn", additional_assets=job.get("additionalAssets", []))
-        LOG.info("Generating heatMap")
-        heat_map["heatMap"] = dashboard_utils.generate_heatmap(evaluated_results)
-
-        flat_heatmap = dict_utils.flatten_data(heat_map)
-    except Exception as eval_ex:
-        heat_map = {"heatMap": {}}
-        LOG.error(str(eval_ex))
-        execution_errors_array.append(
-            "Something went wrong during DMN evaluation or heatmap generation, please check logs"
-        )
-
-    dashboard_result = {"createdDate": datetime.now().strftime("%m/%d/%Y %H:%M:%S")}
-    dashboard_result.update(results_with_defaults)
-    dashboard_result.update(dashboard_result)
-    dashboard_result.update(heat_map)
-    dashboard_result.update(flat_heatmap)
-    dashboard_result.update({"executionErrors": execution_errors_array})
-    dashboard_result.update({"executionErrorsCount": len(execution_errors_array)})
-
+    summary_results = '''
+    {
+    "summary results":"this is a summary result"
+    }  
+    '''
+    custom_cards = '''
+    {
+      "customCards": [
+        {
+          "name":"# of Models",
+          "value":"1"
+        }, {
+          "name":"# of Reports",
+          "value":"1"
+        }, {
+          "name":"# of Pending Reports",
+          "value":"1"
+        }
+      ]
+    }  
+    '''
+    custom_cols = '''
+    {
+      "customColumns": [
+        {
+          "name": "Reporting Quarter",
+          "value": "FY23 Q2"
+        },
+        {
+          "name":"Overall Rating",
+          "value":"Use Caution"
+        }
+      ]
+    }  
+    '''
+    heatmap = '''
+    {
+      "heatMap": {
+        "Overall Assessment": {
+          "testResult": "Green"
+        },
+        "Primary Metric 1": {
+          "testResult": "Green"
+        },
+        "Primary Metric 2": {
+          "testResult": "Green"
+        },
+        "Primary Metric 3": {
+          "testResult": "Green"
+        },
+        "Primary Metric 4": {
+          "testResult": "Green"
+        },
+        "Primary Metric 5": {
+          "testResult": "Green"
+        }
+      }
+    }
+    '''
+    flat_heatmap = '''
+    {
+    "summary results":"this is a summary result"
+    }  
+    '''
+    dashboard_result.update(json.loads(summary_results))
+    dashboard_result.update(json.loads(custom_cards))
+    dashboard_result.update(json.loads(custom_cols))
+    dashboard_result.update(json.loads(heatmap))
+    dashboard_result.update(json.loads(flat_heatmap))
     yield dashboard_result
-
 
 def extract_model_fields(execution_errors_array):
     try:
@@ -140,3 +224,65 @@ def combine_model_test_results(mtrs) -> dict:
         if mtr is not None:
             result.update(mtr.get("testResults", {}))
     return result
+
+
+def calculate_risk_count():
+    risk = DEPLOYABLE_MODEL.get("storedModel", {}).get("modelMetaData", {}).get("modelRisk", "0")
+    try:
+        int(risk[-1])
+    except:
+        risk = "0"
+    t1 = 1 <= int(risk[-1]) <= 3
+    t4 = 4 <= int(risk[-1]) <= 5
+    return {"risk_1_2_3": 1 if t1 else 0, "risk_4_5": 1 if t4 else 0}
+
+
+class ValueEntry(Dict):
+    def __init__(self, key: str, value, color: str = None):
+        super().__init__()
+        if color is not None:
+            self.update({"key": key, "value": value, "color": color})
+        else:
+            self.update({"key": key, "value": value})
+
+
+class CustomCard(Dict):
+    def __init__(self, value: List[ValueEntry], name: str = "Custom Card"):
+        super().__init__()
+        self.update({"name": name, "value": value})
+
+
+class preformatted(str):
+    def __init__(self, val):
+        super().__init__()
+
+
+def generate_custom_entry(value_list,
+                          sub_column_list: Optional[List[str]] = None,
+                          color_list: Optional[List[str]] = None,
+                          name: Optional[str] = "CustomCard") -> CustomCard:
+    entries = []
+    for i in range(len(value_list)):
+        if type(value_list[i]) is preformatted:
+            val = value_list[i]
+        else:
+            val = parse_number(value_list[i])
+        color = None
+        if color_list is not None:
+            color = color_list[i]
+        entries.append(ValueEntry(sub_column_list[i], val, color))
+    return CustomCard(entries, name=name)
+
+
+def calculate_custom_cards(cards: List[CustomCard]):
+    return {"customCards": cards}
+
+
+def calculate_custom_columns(columns: List[CustomCard]):
+    return {"customColumns": columns}
+
+
+def parse_number(value):
+    if value is None:
+        return "N/A"
+    return float(re.sub(r'[^\w.]', '', str(value)))
