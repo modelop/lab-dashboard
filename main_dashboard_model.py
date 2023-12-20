046c07b3fe558e7961816e7ea60bedb31785f9b7
Dave Trier <DaveTrier@modelopdminsmbp.lan>
2023-12-20 08:40:04 -0700

    added custom cards and changed the flattened metrics

--- main_dashboard_model.py
+++ main_dashboard_model.py
@@ -16,7 +16,8 @@ from pathlib import Path
 import pandas as pd
 
 LOG = logger.configure_logger()
-RATING = {}
+RATING = ""
+QUARTER = ""
 
 heatMapColors = ["Gray", "Yellow", "Green", "Green", "Green", "Green", "Red"]
 aryOrgs = ["Insurance","Residuals","Credit","Fraud","AML","Insider Threats","Marketing","Trade Analytics","Capital Markets","Asset Management","Payments","Lockbox OCR"]
@@ -36,7 +37,9 @@ def generate_rating() -> str:
     RATING = aryRatings[random.randint(0,len(aryRatings) - 1)]
     return RATING
 def generate_quarter() -> str:
-    return aryQuarter[random.randint(0,len(aryQuarter) - 1)]
+    global QUARTER
+    QUARTER = aryQuarter[random.randint(0,len(aryQuarter) - 1)]
+    return QUARTER
 def generate_category() -> str:
     return ""
 
@@ -64,97 +67,13 @@ def init(job_json):
 
 # modelop.metrics
 def metrics(baseline, comparator) -> dict:
-    # result = {}
-    # heat_map = {}
-    # flat_heatmap = {}
-    # execution_errors_array = []
-    # results_with_defaults = {
-    #     "CSI_maxCSIValue": -99,
-    #     "auc": -99,
-    #     "r2_score": 99,
-    #     "Bias_maxPPRDisparityValue": -99,
-    #     "Bias_minPPRDisparityValue": -99,
-    #     "DataDrift_maxKolmogorov-SmirnovPValue": -99,
-    #     "ConceptDrift_maxKolmogorov-SmirnovPValueValue": -99
-    # }
-
-    # client = moc_client.MOCClient()
-    # mtr_summary_api = model_manage.ModelTestResultSummariesApi(client)
-    # mtr_api = model_manage.ModelTestResultsApi(client)
-    # mlc_api = mlc.MLCApi(client)
-
-    # def get_associated_model_latest_mtrs_mapper(model_association):
-    #     associated_model_id = uuid.UUID(model_association.get("associatedModel", {}).get("id"))
-    #     associated_model_name = model_association.get("associatedModel", {}).get("storedModel", {}).get("modelMetaData", {}).get("name", "No name found")
-    #     response_of_model_test_result_summaries = mtr_summary_api.find_all_by_optional(
-    #         deployable_model_id=uuid.UUID(DEPLOYABLE_MODEL.get("id")),
-    #         associated_model_snapshot_id=associated_model_id,
-    #         sort="createdDate,desc", page=0, size=1)
-    #     if len(response_of_model_test_result_summaries.get("_embedded", {}).get("modelTestResultSummaries", [])) > 0:
-    #         mtr_summary = response_of_model_test_result_summaries.get("_embedded", {}).get("modelTestResultSummaries", [])[0]
-    #         mtr = mtr_api.get(mtr_summary.get("id"))
-    #         return mtr
-    #     else:
-    #         LOG.error(f"Couldn't find Model Test Result for deployableModel " + str(DEPLOYABLE_MODEL.get("id")) +
-    #                   " and associated model " + str(associated_model_name) + " (" + str(associated_model_id) + ")")
-    #         return None
-
-    # LOG.info("Number of associated models in the business model: " + str(len(DEPLOYABLE_MODEL.get("associatedModels", []))))
-    # if len(DEPLOYABLE_MODEL.get("associatedModels", [])) == 0:
-    #     raise Exception("No associated models were found in the current business model ")
-
-    # mtrs = list(map(get_associated_model_latest_mtrs_mapper, DEPLOYABLE_MODEL.get("associatedModels", [])))
-    # mtrs = list(filter(None, mtrs))
-    # result = {}
-    # result.update(combine_model_test_results(mtrs))
-    # result.update(extract_model_fields(execution_errors_array))
-
-    # try:
-    #     results_with_defaults.update(result)
-    #     LOG.info("Performing DMN evaluation")
-    #     job = json.loads(JOB.get("rawJson", {}))
-    #     evaluated_results = mlc_api.evaluate_results(results_with_defaults, "dashboard_model.dmn", additional_assets=job.get("additionalAssets", []))
-    #     LOG.info("Generating heatMap")
-    #     heat_map["heatMap"] = dashboard_utils.generate_heatmap(evaluated_results)
-
-    #     flat_heatmap = dict_utils.flatten_data(heat_map)
-    # except Exception as eval_ex:
-    #     heat_map = {"heatMap": {}}
-    #     LOG.error(str(eval_ex))
-    #     execution_errors_array.append(
-    #         "Something went wrong during DMN evaluation or heatmap generation, please check logs"
-    #     )
-
-    # value_risk = calculate_risk_count()
-
-    # dashboard_result = {"createdDate": datetime.now().strftime("%m/%d/%Y %H:%M:%S")}
-    # dashboard_result.update(results_with_defaults)
-    # dashboard_result.update(dashboard_result)
-    # dashboard_result.update(heat_map)
-    # dashboard_result.update(flat_heatmap)
-    # dashboard_result.update({"executionErrors": execution_errors_array})
-    # dashboard_result.update({"executionErrorsCount": len(execution_errors_array)})
-
-    # dashboard_result.update(calculate_custom_columns([
-    #     generate_custom_entry([preformatted(DEPLOYABLE_MODEL.get("metaData", {}).get("modelStage", "None"))],
-    #                           ["Model Stage"],
-    #                           None,
-    #                           name="Approved Use")
-    # ]))
-
-    #yield dashboard_result
-
+    global RATING
+    global QUARTER
 
     dashboard_result = {
-        "createdDate": datetime.now().strftime('%m/%d/%Y %H:%M:%S'),
-        "modelUseCategory": generate_category(),
-        "modelOrganization": generate_org(),
+        "Dashboard Updated Date": datetime.now().strftime('%m/%d/%Y %H:%M:%S'),
     }
-    summary_results = '''
-    {
-    "summary results":"this is a summary result"
-    }  
-    '''
+
     primary_metrics = {
         "Primary Metrics": [
             {"":1,"Metric": "Outcome 1", "Value": "False"},
@@ -165,22 +84,19 @@ def metrics(baseline, comparator) -> dict:
         ]
     }
 
-    # custom_cards = '''
-    # {
-    #   "customCards": [
-    #     {
-    #       "name":"# of Models",
-    #       "value":"1"
-    #     }, {
-    #       "name":"# of Reports",
-    #       "value":"1"
-    #     }, {
-    #       "name":"# of Pending Reports",
-    #       "value":"1"
-    #     }
-    #   ]
-    # }  
-    # '''
+    custom_cards = '''
+    {
+      "customCards": [
+        {
+          "name":"# of Models",
+          "value":"1"
+        }, {
+          "name":"# of Reports",
+          "value":"1"
+        }
+      ]
+    }  
+    '''
     custom_cols = {
       "customColumns": [
         {
@@ -188,8 +104,8 @@ def metrics(baseline, comparator) -> dict:
           "value": generate_quarter()
         },
         {
-          "name":"Overall Rating",
-          "value":generate_rating()
+          "name": "Overall Rating",
+          "value": generate_rating()
         }
       ]
     }  
@@ -221,11 +137,19 @@ def metrics(baseline, comparator) -> dict:
     "summary results":"this is a summary result"
     }  
     '''
-    dashboard_result.update(json.loads(summary_results))
     dashboard_result.update(primary_metrics)
-    #dashboard_result.update(json.loads(custom_cards))
+    dashboard_result.update(json.loads(custom_cards))
     dashboard_result.update(custom_cols)
     dashboard_result.update(heatmap)
+
+    summary_results = {
+        "Model Organization": generate_org(),
+        "Last Report Quarter": QUARTER,
+        "Last Report Rating": RATING,
+        "Last Report": "https://mocaasin.modelop.center/#/models/business-models/snapshot/ee2542c4-8b49-40f9-b80a-21ab09e70781/monitoring/tests/97c835e8-b2b6-42e5-96b1-939460b449fc"
+    }
+
+    dashboard_result.update(summary_results)
     dashboard_result.update(json.loads(flat_heatmap))
     yield dashboard_result
 
